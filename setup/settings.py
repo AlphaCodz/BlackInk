3326168dd232f09c3ee2d0f3014de0391b235528
AlphaCodz <adeyanjuabiona69@gmail.com>
2023-05-25 20:56:33 +0100

    CI #5 Resolve

--- setup/settings.py
+++ setup/settings.py
@@ -11,13 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os, environ
+import os
+from environ import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-environ.Env().read_env()
+env = Env()
+# environ.Env().read_env()
 
 
 # Quick-start development settings - unsuitable for production
